d4c4aca9addcbbd2f5e5aef79feb8ed482de4ecc
Joshua Marcano <jmarcano617@gmail.com>
2022-06-05 18:00:35 +0000

    created tableRows with list of tabe row html and added tutorial folder

--- book-scraper/extractData.js
+++ book-scraper/extractData.js
@@ -4,18 +4,20 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://news.ycombinator.com/");
+    const tableRows = page.$$eval("tr.athing", (rows) =>
+      rows.map((row) => row.innerHTML))
 
-  let articleTitles = page.$$eval("a.titlelink", (titles) =>
-    titles.map((item) => item.textContent)
-  );
+  // let articleTitles = page.$$eval("a.titlelink", (titles) =>
+  //   titles.map((item) => item.textContent)
+  // );
 
-  let articleLinks = page.$$eval("a.titlelink", (links) =>
-    links.map((item) => item.href)
-  );
-  // edge case: some articles have 0 votes
-  let articleUpVotes = page.$$eval("span.score", (spans) =>
-    spans.map((item) => (item.textContent))
-  );
-  console.log(await articleUpVotes);
+  // let articleLinks = page.$$eval("a.titlelink", (links) =>
+  //   links.map((item) => item.href)
+  // );
+  // // edge case: some articles have 0 votes
+  // let articleUpVotes = page.$$eval("span.score", (spans) =>
+  //   spans.map((item) => (item.textContent))
+  // );
+  console.log(await tableRows);
   await browser.close();
 })();
